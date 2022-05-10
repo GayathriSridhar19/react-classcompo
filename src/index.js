aa8920332a744a48767c8e36976a546579f580ee
GayathriSridhar19 <gayathripragya.vt@gmail.com>
2022-05-10 18:04:28 +0530

    react1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Assessment extends React.Component
+{
+constructor(props)
+{
+  super(props)
+
+   this.state={value:""}
+}
+
+ 
+  oninputchange = (event) => {
+    this.setState({
+      value : event.target.value
+    })
+  }
+
+  render()
+  {
+    
+    return( 
+      
+      <div className='prebox'>
+      <textarea name='box1' value={this.state.box1} onChange={this.oninputchange} className='firstbox' type='text'></textarea>
+      <textarea value={this.state.value} className='secondbox' type='text' readOnly></textarea></div>
+    );
+  }
+} 
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Assessment/>);
+  
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
